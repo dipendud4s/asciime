6a6d1e305c1ebc20709a0730fd5c13c6d6a14374
Dipendu Das <dipendu@Dipendus-MacBook-Pro-2.local>
2022-07-02 17:21:27 +0530

    video streaming added

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ body.appendChild(canvas);
 
 const ctx = canvas.getContext('2d');
 
+// creating image
 const img = new Image();
 img.src = imgUrl;
 const ratio = img.height/img.width;
@@ -21,11 +22,54 @@ img.height = ratio * 1500;
 
 let size = 15;
 let effect;
-img.onload = function init() {
-  canvas.width = img.width;
-  canvas.height = img.height;
-  effect = new AsciiEffect(ctx, img, img.width, img.height);
-  ctx.font = size + 'px Verdana';
+ctx.font = size + 'px Verdana';
+
+// drawing image
+// img.onload = function init() {
+//   canvas.width = img.width;
+//   canvas.height = img.height;
+//   effect = new AsciiEffect(ctx, img, img.width, img.height);
+//   ctx.font = size + 'px Verdana';
+//   effect.draw(size);
+//   console.log(effect)
+// };
+
+
+
+// creating video
+const video = document.createElement("video");
+video.setAttribute("playsinline", "");
+video.setAttribute("autoplay", "");
+video.setAttribute("muted", "");
+video.style.width = "1500px";
+video.style.height = "1500px";
+
+const facingMode = "user";
+const constraints = {
+  audio: false,
+  video: {
+    facingMode
+  }
+};
+
+function draw(video, context, width, height) {
+
+  effect = new AsciiEffect(ctx, video, width, height);
   effect.draw(size);
-  console.log(effect)
+//   console.log(effect)
+
+
+  // context.clearRect (0, 0, width, height);
+  // context.drawImage(video, 0, 0, width, height);
+  setTimeout(draw, 10, video, context, width, height);
 }
+
+navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+  video.srcObject = stream;
+});
+
+canvas.width = 1600;
+canvas.height = 900;
+video.addEventListener('play', function () {
+  draw(this, ctx, canvas.width, canvas.height);
+}, false);
